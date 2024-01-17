c4184635a550d495a375d538bd564278554d618e
Isra <iwmartnz@gmail.com>
2024-01-17 11:52:39 -0600

    Added tailwind and daysi UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Urbanist Variable"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["business"],
+  },
+  plugins: [require("daisyui")],
 };
